2bbc6f388039cbdb6b97b8236b1a1666b57f8cc8
Karuta23 <50730505+Karuta2000@users.noreply.github.com>
2023-06-24 19:07:50 +0200

    homepage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Oauth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
 });
 
 Route::get('/dashboard', function () {
@@ -29,3 +30,54 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+/*Route::get('/', [UserController::class, 'dashboard']);
+
+Route::get('/tasks', [TaskController::class, 'index'])->name('tasks');
+
+Route::get('/tasks/create',  [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks',  [TaskController::class, 'store'])->name('tasks.store');
+Route::get('/tasks/{task}',  [TaskController::class, 'show'])->name('tasks.show');
+Route::get('/tasks/{task}/edit',  [TaskController::class, 'edit'])->name('tasks.edit');
+Route::put('/tasks/update', [TaskController::class, 'update'])->name('tasks.update');
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::put('/tasks/{task}/update-status', [TaskController::class, 'complete'])->name('tasks.complete');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/user/settings', [UserController::class, 'showSettings'])->name('user.settings');
+    Route::put('/user/update', [UserController::class, 'update'])->name('user.update');
+});
+
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects');
+Route::get('/projects/find', [ProjectController::class, 'search'])->name('projects.search');
+Route::get('/projects/create',  [ProjectController::class, 'create'])->name('projects.create');
+Route::post('/projects',  [ProjectController::class, 'store'])->name('projects.store');
+Route::get('/projects/{project}',  [ProjectController::class, 'show'])->name('projects.show');
+Route::get('/projects/{project}/edit',  [ProjectController::class, 'edit'])->name('projects.edit');
+Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
+*/
+Route::get('/auth/google', 'App\Http\Controllers\Oauth\LoginController@redirectToGoogle')->name('login.google');
+Route::get('/auth/google/callback', 'App\Http\Controllers\Oauth\LoginController@handleGoogleCallback');
+
+/*
+Route::get('/notes', [NoteController::class, 'index'])->name('notes');
+Route::get('/notes/create',  [NoteController::class, 'create'])->name('notes.create');
+Route::post('/notes',  [NoteController::class, 'store'])->name('notes.store');
+Route::get('/notes/{note}',  [NoteController::class, 'show'])->name('notes.show');
+Route::get('/notes/{note}/edit',  [NoteController::class, 'edit'])->name('notes.edit');
+Route::put('/notes/{note}', [NoteController::class, 'update'])->name('notes.update');
+Route::delete('/notes/{note}', [NoteController::class, 'destroy'])->name('notes.destroy');
+
+Route::get('/tags', [TagController::class, 'index'])->name('tags');
+Route::get('/tags/create',  [TagController::class, 'create'])->name('tags.create');
+Route::post('/tags',  [TagController::class, 'store'])->name('tags.store');
+Route::get('/tags/{tag}',  [TagController::class, 'show'])->name('tags.show');
+Route::get('/tags/{tag}/edit',  [TagController::class, 'edit'])->name('tags.edit');
+Route::put('/tags', [TagController::class, 'update'])->name('tags.update');
+Route::delete('/tags/{tag}', [TagController::class, 'destroy'])->name('tags.destroy');
+
+Route::post('/tags/{project}',  [TagController::class, 'attachTagsToProject'])->name('tags.attachToProject');
+
+*/
